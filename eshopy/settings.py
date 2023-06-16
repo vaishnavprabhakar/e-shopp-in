318066a6cd3fe9a03b83c923aac2748ab77b3981
vaishnavprabhakar <vaishnavprabhakarkoo@gmail.com>
2023-06-17 00:24:55 +0530

    settings modified

--- eshopy/settings.py
+++ eshopy/settings.py
@@ -14,6 +14,7 @@ from decouple import config, Csv
 from pathlib import Path
 import os
 import product
+import decouple
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET')
+SECRET_KEY = config("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)

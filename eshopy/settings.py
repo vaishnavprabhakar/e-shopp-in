01c0985674f4a6f9d6efe2fc660efcb20cc202ed
vaishnavprabhakar <vaishnavprabhakarkoo@gmail.com>
2023-06-30 13:57:57 +0530

    settings

--- eshopy/settings.py
+++ eshopy/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = '/static' 
+STATIC_ROOT = os.path.join(BASE_DIR, '/static') 
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
@@ -160,7 +160,7 @@ DEFAULT_FROM_EMAIL = config('DEFAULTFROMEMAIL')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
 TEMPLATES_DIRS = [[BASE_DIR] + STATICFILES_DIRS]
 
